4c840e7b90c64a1f0b82a1bf1533ddf0b39ae14a
Mark Tolmacs <tolmi@lazycat.hu>
2013-11-18 09:57:01 +0100

    Gradle updated to 1.8

--- gradle/tools/chocolateyInstall.ps1
+++ gradle/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-Install-ChocolateyZipPackage 'gradle' 'http://services.gradle.org/distributions/gradle-1.6-bin.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'gradle' 'http://services.gradle.org/distributions/gradle-1.8-bin.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 #------- ADDITIONAL SETUP -------#
 $chocolateyToolPath = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) '\gradle-1.6\bin\'
