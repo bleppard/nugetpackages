d117d174120f20bfe407fed7bce9cdf9710cffa8
bleppard <GitHub@public.leppard.com>
2014-01-11 01:58:14 -0500

    Updated Path for Gradle to 1.8
    The path for pointed to gradle-1.6 which doesn't exist since gradle is installed to gradle-1.8

--- gradle/tools/chocolateyInstall.ps1
+++ gradle/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 Install-ChocolateyZipPackage 'gradle' 'http://services.gradle.org/distributions/gradle-1.8-bin.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 #------- ADDITIONAL SETUP -------#
-$chocolateyToolPath = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) '\gradle-1.6\bin\'
+$chocolateyToolPath = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) '\gradle-1.8\bin\'
 Install-ChocolateyPath $chocolateyToolPath 'user'
